6ef03f20c2e006cfe599451585b45d2e67d6f73b
jdfsfs <eelco.wuijts@conclusion.nl>
2021-09-29 13:47:07 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import DiscoverMoviesPage from "./pages/DiscoverMoviesPage";
+import AboutPage from "./pages/AboutPage";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/discover">Discover</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav> */}
+      <NavBar />
+      {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/discover">{DiscoverMoviesPage}</Route>
+        <Route path="/about">{AboutPage}</Route>
+        <Route path="/">{HomePage}</Route>
+      </Switch>
     </div>
   );
 }
